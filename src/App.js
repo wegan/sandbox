b99049377d2c9d2633c6acfb5f8c2a51da475849
Walter Egan <>
2021-01-21 11:07:20 +0100

    first react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+
+    function Header(){
+      return (
+
+        <section><h1>Hello World!</h1></section>
+        
+
+      );
+
+
+    }
+
+    function Main (){
+      return (
+        <section><p>This is my second React component</p></section>
+        
+
+      );
+      
+
+    }
+
+    
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main/>
+
     </div>
   );
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
