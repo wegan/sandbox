6923c6fc404f4646a687566b96d0dc429329c009
Walter Egan <>
2021-01-27 19:16:52 +0100

    more changes

--- zjetdatamodel/JET_Web_Application/src/js/viewModels/dashboard.js
+++ zjetdatamodel/JET_Web_Application/src/js/viewModels/dashboard.js
@@ -7,24 +7,36 @@
 /*
  * Your customer ViewModel code goes here
  */
+// define(
+//   ['accUtils',
+//    'knockout',
+//    'jquery',
+//    'ojs/ojarraydataprovider',
+//    'ojs/ojlabel',
+//    'ojs/ojchart',
+//    'ojs/ojlistview',
+//    'ojs/ojavatar'
+//   ],
 define(
   ['accUtils',
-   'knockout',
-   'jquery',
-   'ojs/ojarraydataprovider',
-   'ojs/ojlabel',
-   'ojs/ojchart',
-   'ojs/ojlistview',
-   'ojs/ojavatar'
+    'knockout',
+    'ojs/ojmodel',
+    'ojs/ojcollectiondataprovider',
+    'ojs/ojarraydataprovider',
+    'ojs/ojlabel',
+    'ojs/ojchart',
+    'ojs/ojlistview',
+    'ojs/ojavatar'
   ],
-  function (accUtils, ko, $, ArrayDataProvider) {
+  // function (accUtils, ko, $, ArrayDataProvider) {
+  function (accUtils, ko, Model, CollectionDataProvider) {
 
     function DashboardViewModel() {
       var self = this;  //generated code
 
       /**
        * Declare observables and read data from JSON file
-       */ 
+       */
       // Master list and detail list observables
       self.activityDataProvider = ko.observable();   //gets data for Activities list
       self.itemsDataProvider = ko.observable();      //gets data for Items list
@@ -37,43 +49,135 @@ define(
       self.activitySelected = ko.observable(false);
       self.selectedActivity = ko.observable();
       self.firstSelectedActivity = ko.observable();
-      
+
       // Item selection observables
       self.itemSelected = ko.observable(false);
       self.selectedItem = ko.observable();
       self.firstSelectedItem = ko.observable();
 
-      var url = "js/store_data.json";  //defines link to local data file
+      // wegan
+      // var url = "js/store_data.json";  //defines link to local data file
 
       // Get Activity objects from file using jQuery method and a method to return a Promise
-      $.getJSON(url).then(function (data) {
-          // Create variable for Activities list and populate using key attribute fetch
-          var activitiesArray = data;
-          self.activityDataProvider(new ArrayDataProvider(activitiesArray, { keyAttributes: 'id' }));
-        }
-      );
-    
+      // Create variable for Activities list and populate using key attribute fetch
+
+      // wegan
+      // $.getJSON(url).then(function (data) {
+
+      //     var activitiesArray = data;
+      //     self.activityDataProvider(new ArrayDataProvider(activitiesArray, { keyAttributes: 'id' }));
+      //   }
+      // );
+
+      // wegan New function. Step 9 onwards in the tutorial
+      //REST endpoint
+      var RESTurl = "https://apex.oracle.com/pls/apex/oraclejet/lp/activities/";
+      //Single line of data
+      var activityModel = Model.Model.extend({
+        urlRoot: RESTurl,
+        idAttribute: 'id'
+      });
+
+      //Multiple models i.e. multiple lines of data
+      self.myActivity = new activityModel();
+      var activityCollection = new Model.Collection.extend({
+        url: RESTurl,
+        model: self.myActivity,
+        comparator: 'id'
+      });
+
+      /*
+      *An observable called activityDataProvider is already bound in the View file
+      *from the JSON example, so you don't need to update dashboard.html
+      */
+      self.myActivityCol = new activityCollection();
+      self.activityDataProvider(new CollectionDataProvider(self.myActivityCol));
+
+
+
+
+
+
       /**
        * Handle selection from Activities list
        */
       self.selectedActivityChanged = function (event) {
         // Check whether click is an Activity selection or a deselection
         if (event.detail.value.length != 0) {
-            // If selection, populate and display list
-            // Create variable for items list using firstSelectedXxx API from List View
-            var itemsArray = self.firstSelectedActivity().data.items;
-            // Populate items list using DataProvider fetch on key attribute
-            self.itemsDataProvider(new ArrayDataProvider(itemsArray, { keyAttributes: 'id' }))
-            // Set List View properties
-            self.activitySelected(true);
-            self.itemSelected(false);
-            // Clear item selection
-            self.selectedItem([]);
-            self.itemData();
+          // If selection, populate and display list
+          // Create variable for items list using firstSelectedXxx API from List View
+          // wegan 
+          // var itemsArray = self.firstSelectedActivity().data.items;
+
+          /**
+           * weganBeginNewEntry
+           */
+          var activityKey = self.firstSelectedActivity().data.id;
+          //REST endpoint for the items list
+          var url = "https://apex.oracle.com/pls/apex/oraclejet/lp/activities/" + activityKey + '/items/';
+
+          function parseItem(response) {
+            var img = 'css/images/product_images/jet_logo_256.png'
+            if (response) {
+              //if the response contains items, pick the first one
+              if (response.items && response.items.length !== 0) { response = response.items[0]; }
+              //if the response contains an image, retain it
+              if (response.image !== null) { img = response['image']; }
+              return {
+                id: response['id'],
+                name: response['name'],
+                price: response['price'],
+                short_desc: response['short_desc'],
+                quantity: response['quantity'],
+                quantity_instock: response['quantity_instock'],
+                quantity_shipped: response['quantity_shipped'],
+                activity_id: response['activity_id'],
+                image: img
+
+
+              };
+            }
+          }
+
+          var itemModel = Model.Model.extend({
+            urlRoot: url,
+            parse: parseItem,
+            idAttribute: 'id'
+          });
+
+          self.myItem = new itemModel();
+          self.itemCollection = new Model.Collection.extend({
+            url: url,
+            model: self.myItem,
+            comparator: 'id'
+          });
+
+          /*
+ *An observable called itemsDataProvider is already bound in the View file
+ *from the JSON example, so you don't need to update dashboard.html
+ */
+          self.myItemCol = new self.itemCollection();
+          self.itemsDataProvider(new CollectionDataProvider(self.myItemCol));
+
+
+          /**
+           * weganEndEntry
+           */
+
+          // Populate items list using DataProvider fetch on key attribute
+          
+          //? OBE should have commented out following line?
+                    // self.itemsDataProvider(new ArrayDataProvider(itemsArray, { keyAttributes: 'id' }))
+          // Set List View properties
+          self.activitySelected(true);
+          self.itemSelected(false);
+          // Clear item selection
+          self.selectedItem([]);
+          self.itemData();
         } else {
           // If deselection, hide list
-           self.activitySelected(false);
-           self.itemSelected(false);
+          self.activitySelected(false);
+          self.itemSelected(false);
         }
       };
 
@@ -83,32 +187,32 @@ define(
       self.selectedItemChanged = function (event) {
         // Check whether click is an Activity Item selection or deselection
         if (event.detail.value.length != 0) {
-            // If selection, populate and display Item details
-            // Populate items list observable using firstSelectedXxx API
-            self.itemData(self.firstSelectedItem().data);
-            // Create variable and get attributes of the items list to set pie chart values
-            var pieSeries = [
-              { name: "Quantity in Stock", items: [self.itemData().quantity_instock] },
-              { name: "Quantity Shipped", items: [self.itemData().quantity_shipped] }
-            ];
-            // Update the pie chart with the data
-            self.pieSeriesValue(pieSeries);
-            self.itemSelected(true);
+          // If selection, populate and display Item details
+          // Populate items list observable using firstSelectedXxx API
+          self.itemData(self.firstSelectedItem().data);
+          // Create variable and get attributes of the items list to set pie chart values
+          var pieSeries = [
+            { name: "Quantity in Stock", items: [self.itemData().quantity_instock] },
+            { name: "Quantity Shipped", items: [self.itemData().quantity_shipped] }
+          ];
+          // Update the pie chart with the data
+          self.pieSeriesValue(pieSeries);
+          self.itemSelected(true);
         } else {
           // If deselection, hide list
-           self.itemSelected(false);
+          self.itemSelected(false);
         }
       };
 
 
       // The following 3 functions are not addressed in this tutorial.
-	  
+
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
 
       /**
        * This section is standard navdrawer starter template code
-       */ 
+       */
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
 
@@ -142,11 +246,11 @@ define(
       };
     }
 
-      /*
-       * Returns a constructor for the ViewModel so that the ViewModel is constructed
-       * each time the view is displayed.  Return an instance of the ViewModel if
-       * only one instance of the ViewModel is needed.
-       */
-      return new DashboardViewModel();
+    /*
+     * Returns a constructor for the ViewModel so that the ViewModel is constructed
+     * each time the view is displayed.  Return an instance of the ViewModel if
+     * only one instance of the ViewModel is needed.
+     */
+    return new DashboardViewModel();
   }
 );
